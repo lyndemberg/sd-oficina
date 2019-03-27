1d9b1c87a7951ab5d4f8cb6ff251c44b1edbd9f6
lyndemberg <lyndembergbatista@outlook.com>
2019-03-27 20:07:22 -0300

    configuraçao do maven ajustada

---
+++ oficina-webapp/src/test/java/sd/oficina/oficinawebapp/OficinaWebApplicationTests.java
@@ -0,0 +1,16 @@
+package sd.oficina.oficinawebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OficinaWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
