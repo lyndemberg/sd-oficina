b74cbd4819ebc76d8ace981920458c14f6830651
Lucas Duete <lm.duete@gmail.com>
2019-03-31 20:37:14 -0300

    Refatorado Dockerfile do postgres para inserir script que crie os DB de cada serviço

--- customer1/src/main/java/sd/oficina/customer1/exceptions/AtributoIdInvalidoException.java
+++ customer1/src/main/java/sd/oficina/customer1/exceptions/AtributoIdInvalidoException.java
@@ -1,14 +1,12 @@
 package sd.oficina.customer1.exceptions;
 
-import com.sun.istack.internal.NotNull;
-
 public class AtributoIdInvalidoException extends RuntimeException {
 
     public AtributoIdInvalidoException() {
         super("Um atributo de Id invalido (null or <= 0) foi passado para o service");
     }
 
-    public AtributoIdInvalidoException(@NotNull final String message) {
+    public AtributoIdInvalidoException(final String message) {
         super(message);
     }
 }
--- customer1/src/main/java/sd/oficina/customer1/exceptions/TentaPersistirObjetoNullException.java
+++ customer1/src/main/java/sd/oficina/customer1/exceptions/TentaPersistirObjetoNullException.java
@@ -1,14 +1,12 @@
 package sd.oficina.customer1.exceptions;
 
-import com.sun.istack.internal.NotNull;
-
 public class TentaPersistirObjetoNullException extends RuntimeException {
 
     public TentaPersistirObjetoNullException() {
         super("Nao e possivel persistir um objeto nulo");
     }
 
-    public TentaPersistirObjetoNullException(@NotNull final String message) {
+    public TentaPersistirObjetoNullException(final String message) {
         super(message);
     }
 }
