4298b8ed60d8879c6de1b0fc93e735a8fc3e7b3c
alexalins <alexa.glinsoo@gmail.com>
2019-04-04 14:19:57 -0300

    implementando costomer3

--- customer3/src/main/java/sd/oficina/customer3/grpc/ModeloService.java
+++ customer3/src/main/java/sd/oficina/customer3/grpc/ModeloService.java
@@ -1,10 +1,10 @@
-package sd.oficina.customer2.grpc;
+package sd.oficina.customer3.grpc;
 
-import io.grpc.stub.StreamObserver;
-import sd.oficina.customer2.dao.ModeloDAO;
-import sd.oficina.shared.model.customer.Modelo;
 import com.google.protobuf.Empty;
+import io.grpc.stub.StreamObserver;
+import sd.oficina.customer3.dao.ModeloDao;
 import sd.oficina.shared.converter.ProtoConverterCustomer;
+import sd.oficina.shared.model.customer.Modelo;
 import sd.oficina.shared.proto.customer.ModeloProto;
 import sd.oficina.shared.proto.customer.ModeloProtoList;
 import sd.oficina.shared.proto.customer.ModeloResult;
@@ -12,61 +12,67 @@ import sd.oficina.shared.proto.customer.ModeloServiceGrpc;
 
 import java.util.List;
 
-public class ModeloImpl extends ModeloServiceGrpc.ModeloServiceImplBase {
+public class ModeloService extends ModeloServiceGrpc.ModeloServiceImplBase {
 
-    private ModeloDAO dao;
+    private ModeloDao dao;
 
-    public ModeloImpl(){
-        dao = new ModeloDAO();
-    }
-
-    @Override
-    public void buscarTodos(Empty request, StreamObserver<ModeloProtoList> responseObserver) {
-        List<Modelo> modelos = dao.todos();
-        final ModeloProtoList.Builder builder = ModeloProtoList.newBuilder();
-        modelos.forEach(f-> builder.addModelos(ProtoConverterCustomer.modelToProto(f)));
-        responseObserver.onNext(builder.build());
-        responseObserver.onCompleted();
+    public ModeloService() {
+        this.dao = new ModeloDao();
     }
 
     @Override
     public void salvar(ModeloProto request, StreamObserver<ModeloResult> responseObserver) {
-        Modelo result = this.dao.salvar(ProtoConverterCustomer.protoToModel(request));
-        ModeloResult modeloResult = ModeloResult.newBuilder()
+        Modelo resultado = this.dao.salvar(ProtoConverterCustomer.protoToModel(request));
+        //
+        ModeloResult modelo = ModeloResult.newBuilder()
                 .setCodigo(200)
-                .setModelo(ProtoConverterCustomer.modelToProto(result))
+                .setModelo(ProtoConverterCustomer.modelToProto(resultado))
                 .build();
-        responseObserver.onNext(modeloResult);
+        //
+        responseObserver.onNext(modelo);
         responseObserver.onCompleted();
     }
 
     @Override
     public void atualizar(ModeloProto request, StreamObserver<ModeloResult> responseObserver) {
-        Modelo modelo = dao.atualizar(ProtoConverterCustomer.protoToModel(request));
+        Modelo modelo = this.dao.atualizar(ProtoConverterCustomer.protoToModel(request));
+        //
         responseObserver.onNext(ModeloResult
                 .newBuilder()
                 .setModelo(
                         modelo != null ? ProtoConverterCustomer.modelToProto(modelo) : ModeloProto.newBuilder().build()
                 )
                 .build());
+        //
         responseObserver.onCompleted();
     }
 
     @Override
     public void deletar(ModeloProto request, StreamObserver<ModeloResult> responseObserver) {
-        this.dao.deletar(request.getId());
+        this.dao.remover(request.getId());
         responseObserver.onNext(ModeloResult.newBuilder().setCodigo(200).build());
         responseObserver.onCompleted();
     }
 
     @Override
     public void buscar(ModeloProto request, StreamObserver<ModeloResult> responseObserver) {
-        Modelo modelo = dao.buscar(request.getId());
+        Modelo modelo = this.dao.getById(request.getId());
         responseObserver.onNext(ModeloResult
                 .newBuilder()
                 .setModelo(
                         modelo != null ? ProtoConverterCustomer.modelToProto(modelo) : ModeloProto.newBuilder().build())
                 .build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void buscarTodos(Empty request, StreamObserver<ModeloProtoList> responseObserver) {
+        List<Modelo> anos = this.dao.getAll();
+        //
+        final ModeloProtoList.Builder builder = ModeloProtoList.newBuilder();
+        anos.forEach(a -> builder.addModelos(ProtoConverterCustomer.modelToProto(a)));
+        //
+        responseObserver.onNext(builder.build());
         responseObserver.onCompleted();
     }
 }
