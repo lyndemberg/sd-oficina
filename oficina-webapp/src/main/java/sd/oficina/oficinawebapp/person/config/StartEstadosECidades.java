fcb705c0cd987262c58d1648eb9d43787a6baffe
Jozymar <jozimarsoares.js@gmail.com>
2019-04-08 18:09:52 -0300

    Startando estados e cidades com threads

--- oficina-webapp/src/main/java/sd/oficina/oficinawebapp/person/config/StartEstadosECidades.java
+++ oficina-webapp/src/main/java/sd/oficina/oficinawebapp/person/config/StartEstadosECidades.java
@@ -11,12 +11,16 @@ import sd.oficina.shared.model.person.Estado;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
 public class StartEstadosECidades {
 
     @Bean
     public void start() {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
         EstadoService estadoService = new EstadoService();
         CidadeService cidadeService = new CidadeService();
@@ -37,17 +41,21 @@ public class StartEstadosECidades {
 
                 System.out.println("INSERINDO ESTADOS E CIDADES, POR FAVOR AGUARDE!");
 
-                for (EstadoJSON estadoJSON : estados.getEstados()) {
-                    estado.setNome(estadoJSON.getNome());
-                    Estado estadoParaSalvar = estadoService.salvar(estado);
-                    for (String cidadeJSON : estadoJSON.getCidades()) {
-                        cidade.setNome(cidadeJSON);
-                        cidade.setEstado(estadoParaSalvar);
-                        cidadeService.salvar(cidade);
+                Runnable runnable = () -> {
+                    for (EstadoJSON estadoJSON : estados.getEstados()) {
+                        estado.setNome(estadoJSON.getNome());
+                        Estado estadoParaSalvar = estadoService.salvar(estado);
+                        for (String cidadeJSON : estadoJSON.getCidades()) {
+                            cidade.setNome(cidadeJSON);
+                            cidade.setEstado(estadoParaSalvar);
+                            cidadeService.salvar(cidade);
+                        }
                     }
-                }
+                };
 
                 System.out.println("INSERÇÕES DE ESTADOS E CIDADES CONCLUÍDAS!");
+
+                executorService.execute(runnable);
 
             }
 
