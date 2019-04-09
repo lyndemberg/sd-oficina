41835fd8848da088d76b79889e3315c08b02d83f
Jozymar <jozimarsoares.js@gmail.com>
2019-04-09 19:50:02 -0300

    Adicionando servidor em store2

--- store2/src/main/java/sd/oficina/store2/Store2Application.java
+++ store2/src/main/java/sd/oficina/store2/Store2Application.java
@@ -1,7 +1,33 @@
 package sd.oficina.store2;
 
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import sd.oficina.store2.grpc.EstoqueService;
+import sd.oficina.store2.grpc.ServicoService;
+
+import java.io.IOException;
+
 public class Store2Application {
     public static void main(String[] args) {
+
+        //
+        System.out.println("Servidor Store 2 inicializado");
+        //
+
+        //Inicializando o server
+        Server server = ServerBuilder
+                .forPort(1111)
+                .addService((BindableService) new EstoqueService())
+                .addService((BindableService) new ServicoService())
+                .build();
+
+        try {
+            server.start();
+            server.awaitTermination();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
