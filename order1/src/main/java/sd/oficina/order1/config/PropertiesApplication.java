651a6f0f1673b77be70b0383ffc9a9d6033dfd85
lyndemberg <lyndembergbatista@outlook.com>
2019-04-10 14:13:20 -0300

    atualizando leitura do properties no serviço order1

--- order1/src/main/java/sd/oficina/order1/config/PropertiesApplication.java
+++ order1/src/main/java/sd/oficina/order1/config/PropertiesApplication.java
@@ -5,6 +5,10 @@ import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import sd.oficina.order1.Order1Application;
+
+import java.io.File;
+import java.net.URISyntaxException;
 
 public class PropertiesApplication {
 
