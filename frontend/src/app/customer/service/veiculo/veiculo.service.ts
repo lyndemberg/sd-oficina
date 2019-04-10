484c30f19924c5c3a4bdc21e531da9262b67053d
alexalins <alexa.glinsoo@gmail.com>
2019-04-10 11:54:18 -0300

    arrumando service customer

--- frontend/src/app/customer/service/veiculo/veiculo.service.ts
+++ frontend/src/app/customer/service/veiculo/veiculo.service.ts
@@ -1,32 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Veiculo } from '../../model/veiculo';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VeiculoService {
 
+  private url: string = environment.API_URL + 'veiculo/';
   constructor(private http: HttpClient) { }
 
-  salvar(Veiculo): Observable<Veiculo> {
-    return this.http.post<Veiculo>('https://localhost:8080/api/veiculo', Veiculo);
+  salvar(veiculo : Veiculo): Observable<HttpResponse<Veiculo>> {
+    return this.http.post<Veiculo>( this.url , veiculo, { observe: 'response' });
   }
 
-  atualizar(Veiculo): Observable<Veiculo> {
-    return this.http.put<Veiculo>('https://localhost:8080/api/veiculo', Veiculo);
+  atualizar(veiculo : Veiculo): Observable<HttpResponse<Veiculo>> {
+    return this.http.put<Veiculo>( this.url , veiculo, { observe: 'response' });
   }
 
-  deletar(id): Observable<any> {
-    return this.http.delete<any>('https://localhost:8080/api/veiculo/' + id);
+  deletar(id : number): Observable<HttpResponse<Veiculo>> {
+    return this.http.delete<any>( this.url + id, { observe: 'response' });
   }
 
-  listarTodos(): Observable<Veiculo[]> {
-    return this.http.get<Veiculo[]>('https://localhost:8080/api/veiculo');
+  listarTodos(): Observable<HttpResponse<Veiculo[]>> {
+    return this.http.get<Veiculo[]>( this.url, { observe: 'response' });
   }
 
-  listarPorId(id): Observable<Veiculo[]> {
-    return this.http.get<Veiculo[]>('https://localhost:8080/api/veiculo/' + id);
+  listarPorId(id : number): Observable<HttpResponse<Veiculo>> {
+    return this.http.get<Veiculo>( this.url + id, { observe: 'response' } );
   }
 }
