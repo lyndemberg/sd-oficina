f5eab545e56133d65c330b1c500f01d339c9c019
alexalins <alexa.glinsoo@gmail.com>
2019-04-12 13:29:31 -0300

    criando metodos no service ordem

--- frontend/src/app/order/service/order.service.ts
+++ frontend/src/app/order/service/order.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { OrdemServico } from '../model/ordemServico';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  private url: string = environment.API_URL + 'ordemservico/';
+  constructor(private http: HttpClient) { }
+
+  salvar(ordem : OrdemServico): Observable<HttpResponse<OrdemServico>>{
+    return this.http.post<OrdemServico>( this.url , ordem, { observe: 'response' });
+  }
+
+  realizarPagamento(ordem : OrdemServico): Observable<HttpResponse<OrdemServico>> {
+    return this.http.put<OrdemServico>( this.url + "pagamento/", ordem, { observe: 'response' });
+  }
+
+  concluirOrdem(ordem : OrdemServico): Observable<HttpResponse<OrdemServico>> {
+    return this.http.put<OrdemServico>( this.url + "concluir/", ordem, { observe: 'response' });
+  }
+
+  listarPorCliente(id : number): Observable<HttpResponse<OrdemServico[]>>  {
+    return this.http.get<OrdemServico[]>( this.url + "cliente/" + id, { observe: 'response' });
+  }
 }
