13e748d646465f7b0d2543417d4b6ebbdc22a124
Jozymar <jozimarsoares.js@gmail.com>
2019-04-13 19:16:37 -0300

    Criando daos de cidade, estado, cliente e fornecedor em person3

---
+++ person3/src/main/java/sd/oficina/person3/daos/CidadeDao.java
@@ -0,0 +1,68 @@
+package sd.oficina.person3.daos;
+
+import sd.oficina.shared.model.person.Cidade;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CidadeDao {
+
+    private EntityManager entityManager =
+                Persistence.createEntityManagerFactory("persistencia")
+                        .createEntityManager();
+
+    public Cidade salvar(Cidade cidade) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(cidade);
+        transaction.commit();
+        return cidade;
+    }
+
+    public Cidade atualizar(Cidade cidade) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.merge(cidade);
+        transaction.commit();
+        return cidade;
+    }
+
+    public void deletar(Cidade cidade) {
+
+        cidade.setEstado(null);
+        cidade = atualizar(cidade);
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        if (!entityManager.contains(cidade)) {
+            cidade = entityManager.merge(cidade);
+        }
+
+        entityManager.remove(cidade);
+        transaction.commit();
+    }
+
+    public Cidade buscar(Cidade cidade) {
+        return entityManager.find(Cidade.class, cidade.getId());
+    }
+
+    public List<Cidade> listar() {
+        TypedQuery<Cidade> query = entityManager
+                .createQuery("SELECT cidade FROM Cidade cidade", Cidade.class);
+        List<Cidade> cidades = query.getResultList();
+        if (cidades == null) {
+            return new ArrayList<>();
+        }
+        List<Cidade> cidadesRecuperadas= new ArrayList<>();
+        for (Cidade cidade : cidades) {
+            cidadesRecuperadas.add(cidade);
+
+        }
+        return cidadesRecuperadas;
+    }
+}
