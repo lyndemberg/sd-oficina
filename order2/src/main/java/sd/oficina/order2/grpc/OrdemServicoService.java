526d091a163b4df22eb6241911ce57be546ab6e7
Lucas Duete <lm.duete@gmail.com>
2019-04-13 19:47:09 -0300

    Adicionado cache para metodo buscarOrdensPorCliente() em Order2

--- order2/src/main/java/sd/oficina/order2/grpc/OrdemServicoService.java
+++ order2/src/main/java/sd/oficina/order2/grpc/OrdemServicoService.java
@@ -2,12 +2,12 @@ package sd.oficina.order2.grpc;
 
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import sd.oficina.order2.dao.OrdemServicoDao;
 import sd.oficina.order2.exceptions.AtributoIdInvalidoException;
-import sd.oficina.order2.exceptions.TentaPersistirObjetoNullException;
-import sd.oficina.shared.converter.ProtoConverterCustomer;
+import sd.oficina.order2.infra.cache.ConnectionFactory;
 import sd.oficina.shared.converter.ProtoConverterOrder;
-import sd.oficina.shared.model.customer.AnoModelo;
 import sd.oficina.shared.model.order.OrdemServico;
 import sd.oficina.shared.proto.order.ClienteProto;
 import sd.oficina.shared.proto.order.OrdemProto;
@@ -16,12 +16,16 @@ import sd.oficina.shared.proto.order.OrderServiceGrpc;
 import java.util.List;
 import java.util.Optional;
 
-public class OrdemServicoService extends OrderServiceGrpc.OrderServiceImplBase{
+public class OrdemServicoService extends OrderServiceGrpc.OrderServiceImplBase {
 
     private OrdemServicoDao ordemServicoDao;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final HashOperations<String, Object, Object> hashOperations;
 
     public OrdemServicoService() {
         this.ordemServicoDao = new OrdemServicoDao();
+        this.redisTemplate = ConnectionFactory.getRedisTemplate();
+        this.hashOperations = redisTemplate.opsForHash();
     }
 
     @Override
@@ -104,11 +108,16 @@ public class OrdemServicoService extends OrderServiceGrpc.OrderServiceImplBase{
 
     @Override
     public void buscarOrdensPorCliente(ClienteProto request, StreamObserver<OrdemProto> responseObserver) {
-        this.ordemServicoDao
-                .buscarPorClienteId(request.getId())
-                .forEach(ordemServico -> responseObserver.onNext(ProtoConverterOrder.modelToProto(ordemServico)));
+        List<OrdemServico> ordemServicos = this.ordemServicoDao.buscarPorClienteId(request.getId());
 
+        // Envia respostas ao cliente
+        ordemServicos.forEach(ordemServico -> responseObserver.onNext(ProtoConverterOrder.modelToProto(ordemServico)));
+
+        // Finaliza comunicaçao
         responseObserver.onCompleted();
+
+        // Apos finalizar a comunicaçao atualiza o Cache
+        hashOperations.put(OrdemServico.class.getSimpleName(), request.getId(), ordemServicos);
     }
 
 }
